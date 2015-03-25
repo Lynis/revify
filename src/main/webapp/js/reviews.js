aa09c4a1ae8dc241aee4661af59cabfbb088f885
kalyanisjsu <kalyaninirmal6@gmail.com>
2015-03-24 22:21:33 -0700

    Adding detailed review page, sorting functionality in review

--- src/main/webapp/js/reviews.js
+++ src/main/webapp/js/reviews.js
@@ -2,17 +2,27 @@
  * Created by kalyaninirmal on 3/22/2015.
  */
 
+var categories = ["","Mobile", "Camera"];
+var selectedCategory;
+var selectedCategoryFeatureArray;
 
-function loadProducts(category)
+
+function loadProducts(category, featuresArray)
 {
-    removeProducts(category);
+    var categoryItems = ["", "Nexus 4", "iPhone 6"];
+    selectedCategory = category;
+    selectedCategoryFeatureArray = featuresArray;
+    removeExistingProductsCards();
+    populateFeatureDropdown(selectedCategoryFeatureArray);
+
     var leftColumn = document.getElementById("leftColumnID");
-    for(var i = 1; i < 3; i++)
+
+    for(var i = 1; i < categoryItems.length; i++)
     {
         //creating card for product and feature
         var cardDiv = document.createElement('div');
         cardDiv.className = "card";
-        cardDiv.id = category+"cardID_"+i;
+        cardDiv.id = "cardID_"+i;
 
         // create left div on card
         var leftDiv = document.createElement('div');
@@ -29,6 +39,7 @@ function loadProducts(category)
         // create feature details card
         var featureDetailsDiv = document.createElement('div');
         featureDetailsDiv.className = "featureDetails";
+        featureDetailsDiv.id = "featureDetailsDiv"+i;
 
         // create left pin
         var pinLeft  = document.createElement('div');
@@ -39,20 +50,29 @@ function loadProducts(category)
         pinRight.className = "pin-right";
 
         // create feature image
-        var featureImage_1 = document.createElement('img');
-        featureImage_1.src = "images/"+category+"_feature1.jpg";
-        featureImage_1.width = 40;
-        featureImage_1.height = 40;
+        for(var j = 0; j < featuresArray.length; j++){
 
-        var featureImage_2 = document.createElement('img');
-        featureImage_2.src = "images/"+category+"_feature2.jpg";
-        featureImage_2.width = 40;
-        featureImage_2.height = 40;
+            var featureImage = document.createElement('img');
+            featureImage.src = "images/"+featuresArray[j]+".jpg";
+            featureImage.id = category+"feature_"+i+j;
+            featureImage.width = 40;
+            featureImage.height = 40;
+            var ratingsImage = document.createElement('img');
 
-        var featureImage_3 = document.createElement('img');
-        featureImage_3.src = "images/"+category+"_feature3.jpg";
-        featureImage_3.width = 40;
-        featureImage_3.height = 40;
+            if(j == 2){
+                ratingsImage.src = "images/dislike.jpg";
+            }
+            else{
+                ratingsImage.src = "images/like.jpg";
+            }
+            ratingsImage.width = 50;
+            ratingsImage.height = 50;
+
+            featureDetailsDiv.appendChild(featureImage);
+            featureDetailsDiv.appendChild(ratingsImage);
+            featureDetailsDiv.appendChild(document.createElement('br'));
+            featureDetailsDiv.appendChild(document.createElement('br'));
+        }
 
         // create product image
         var productImage = document.createElement('img');
@@ -72,20 +92,25 @@ function loadProducts(category)
         pinImageRight.width = 35;
         pinImageRight.height = 35;
 
-        // populate feature list
+        var button = document.createElement('button');
+        button.id = "button_"+i;
+        button.onclick = function(){
+            var w = window.open();
+            w.document.open();
+            w.document.write("<h3>Detailed reviews :</h3><fieldset><legend><img src='images/Battery.jpg' width='100' height='100'</img></legend></br><img src='images/user1.jpg' width='100' height='100'</img><img src='images/review-heart.jpg' width='80' height='80'></br><img src='images/user2.jpg' width='100' height='100'</img><img src='images/review-heart.jpg' width='80' height='80'>" +
+            "<img src='images/review-heart.jpg' width='80' height='80'><img src='images/review-heart.jpg' width='80' height='80'>" +
+            "<img src='images/review-heart.jpg' width='80' height='80'><img src='images/review-heart.jpg' width='80' height='80'></fieldset><br/>" +
+            "<fieldset><legend><img src='images/Camera.jpg' width='100' height='100'</img></legend></br><img src='images/user1.jpg' width='100' height='100'</img><img src='images/review-heart.jpg' width='80' height='80'></br>" +
+            "<img src='images/user2.jpg' width='100' height='100'</img><img src='images/review-heart.jpg' width='80' height='80'></fieldset></br>" +
+            "<fieldset><legend><img src='images/Connectivity.jpg' width='100' height='100'</img></legend><img src='images/user1.jpg' width='100' height='100'</img><img src='images/review-heart.jpg' width='80' height='80'></br><img src='images/user2.jpg' width='100' height='100' </img><img src='images/review-heart.jpg' width='80' height='80'></fieldset>");
+            w.document.close();
+        }
 
+        var buttonText = document.createTextNode("More..");
+        button.appendChild(buttonText);
 
         pinLeft.appendChild(pinImageLeft);
         pinRight.appendChild(pinImageRight);
-
-        featureDetailsDiv.appendChild(featureImage_1);
-        featureDetailsDiv.appendChild(document.createElement('br'));
-        featureDetailsDiv.appendChild(document.createElement('br'));
-        featureDetailsDiv.appendChild(featureImage_2);
-        featureDetailsDiv.appendChild(document.createElement('br'));
-        featureDetailsDiv.appendChild(document.createElement('br'));
-        featureDetailsDiv.appendChild(featureImage_3);
-
 
         featureDiv.appendChild(pinLeft);
         featureDiv.appendChild(pinRight);
@@ -94,6 +119,8 @@ function loadProducts(category)
         leftDiv.appendChild(productImage);
         rightDiv.appendChild(featureDiv);
 
+        leftDiv.appendChild(button);
+
         cardDiv.appendChild(leftDiv);
         cardDiv.appendChild(rightDiv);
 
@@ -101,20 +128,51 @@ function loadProducts(category)
     }
 }
 
-function removeProducts(cat)
+function populateFeatureDropdown(featuresArray){
+
+    removeFeatureDropDownOptions();
+
+    // populate feature list
+
+    for(var i = 0; i < featuresArray.length ; i++){
+        var featureDropdown = document.getElementById('featureDropdown');
+        var opt = document.createElement("option");
+        opt.value = featuresArray[i];
+        featureDropdown.appendChild(opt);
+    }
+}
+
+
+function removeFeatureDropDownOptions(){
+    var featureDropdownList = document.getElementById('featureDropdown').children;
+    if ( featureDropdownList.length > 0 ) {
+        document.getElementById('featureDropdown').removeChild( featureDropdownList[0]);
+        removeFeatureDropDownOptions('featureDropdown');
+    }
+}
+
+function selectedValue(){
+
+    var selectedFeature = document.getElementById('featureDropdownValue');
+    alert(selectedFeature.value);
+    removeFeatures(selectedFeature.value);
+}
+
+function removeFeatures(selectedFeature){
+
+    var index = selectedCategoryFeatureArray.indexOf(selectedFeature);
+    var sortedFeatureArray = selectedCategoryFeatureArray.splice(index,1);
+    loadProducts(selectedCategory, sortedFeatureArray);
+}
+
+function removeExistingProductsCards()
 {
     var cardDiv = document.getElementsByClassName("card");
     var len = cardDiv.length;
-    var category = ["mobile", "camera"];
-    for(var k = 0; k < category.length; k++)
+    for(var i = 1; i<= len; i++)
     {
-        if(category[k]!=cat)
-        {
-            for(var i = 1; i<= len; i++)
-            {
-                var card = document.getElementById(category[k]+"cardID_"+i);
-                card.parentNode.removeChild(card);
-            }
-        }
+        var card = document.getElementById("cardID_"+i);
+        card.parentNode.removeChild(card);
     }
 }
+
