d13c91a6f516ec4485a073b346e7d7e5ba152728
vijayakedar <vijayakedar@gmail.com>
2015-04-27 22:02:53 -0700

    add latest-review page

--- src/main/webapp/js/home.js
+++ src/main/webapp/js/home.js
@@ -21,22 +21,6 @@ var extractBaseUrl = function(){
 }
 
 var categoryOnSuccess = function(response, status, xhr){
-
-    try{
-        var categories = response;
-        var categoryMenu = $('.dl-menu');
-        for(var i in categories){
-            var category = categories[i];
-            var li = "<li><div><img src=\"" + category.icon +"\"><a href=\"\">"+
-                "<span>" + category.categoryName + "</span></a></div></li>";
-
-            categoryMenu.append(li);
-        }
-    }catch(e){
-        console.log(e);
-        alert("Error loading page");
-    }
-
 	try{
 		var categories = response;
 		var categoryMenu = $('.dl-menu');
